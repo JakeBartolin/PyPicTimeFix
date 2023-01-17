94c4a434c4b12c0a41cbff46a0be464451dbf94e
JakeBartolin <105478928+JakeBartolin@users.noreply.github.com>
2023-01-17 00:26:28 -0500

    add welcome message+
    - added welcome message
    - added license info in welcome message
    - created user dialog to gather date info in script (not hardcoded)
    - removed hardcoded date info
    - added a couple docstrings

--- py_pic_time_fix.py
+++ py_pic_time_fix.py
@@ -8,25 +8,6 @@ from exif import Image, DATETIME_STR_FORMAT
 import os
 import datetime
 
-
-# The currently set date and time of one of the pictures.
-CURRENT_DATE = datetime.datetime(
-    year = 2022,
-    month = 1,
-    day = 22,
-    hour = 22,
-    minute = 43,
-    second = 26)
-
-# The date you want to be set to the same picture.
-DESIRED_DATE = datetime.datetime(
-    year = 2022,
-    month = 2,
-    day = 1,
-    hour = 0,
-    minute = 0,
-    second = 0)
-
 # Whether to overwrite the "datedigitized"
 # and "dateoriginal" EXIF fields.
 OVERWRITE_DATEDIGITIZED = True
@@ -44,15 +25,61 @@ def is_file_type_pic(filename):
         return True
     return False
 
-def print_num_of_pics(given_directory):
+def get_num_of_pics_in(given_directory):
     '''Prints the number of picture files in a given directory.'''
     pic_count = 0
     for file in given_directory:
         if is_file_type_pic(file):
             pic_count += 1
-    print(f"{pic_count} picture files were found.")    
+    return pic_count 
+
+def print_welcome_message():
+    print("""\n
+    **************************************************************************************
+    *                                     MIT License                                    *
+    **************************************************************************************
+    
+    Copyright (c) 2023 Jacob Bartolin
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    
+    
+    
+    
+    **************************************************************************************
+    *                             Welcome to Py Pic Time Fix                             *
+    **************************************************************************************
+    
+    This script works by finding the difference between the known (wrong) date
+    of one of your photos and the desired (correct) date of the same photo. Then
+    it will re-date all photos in the same folder as the script by that difference.
+    
+    Before beginning, make sure all the photos you want to change the date on are in
+    the same folder as this script file. Make sure you also have a BACKUP of all the
+    photos in case something goes wrong.
+    
+    """)
 
 def write_metadata(image, exif_field, metadata):
+    """Writes the given metadata to the given EXIF field of the
+    given image object.
+    """
     metadata = metadata.strftime(DATETIME_STR_FORMAT)
     setattr(image, exif_field, metadata)
     print(f"SET {image.name} DATETIME: {metadata}")
@@ -94,25 +121,37 @@ def change_image_datetime(
     with open(image_filename, 'wb') as new_image_file:
         new_image_file.write(image.get_file())
 
+def input_datetime_dialog():
+    """Returns a datetime object after prompting the user to enter
+    the year, month, day, hour, minute and second.
+    """
+    year = input("Please enter the year: ")
+    month = input("Please enter the month: ")
+    day = input("Please enter the day: ")
+    hour = input("Please enter the hour: ")
+    minute = input("Please enter the minute: ")
+    second = input("Please enter the second: ")
+    return datetime.datetime(year, month, day, hour, minute, second)
+
 def main():
-    print_num_of_pics(os.listdir(os.getcwd()))
-    print("Continue? (y/n)")
-    # TODO: Clean this up. (Break into functions?)
-    while True:
-        user_input = input()
-        if user_input == "y":
-            for file in os.listdir(os.getcwd()):
-                if is_file_type_pic(file):
-                    change_image_datetime(
-                        file,
-                        (CURRENT_DATE - DESIRED_DATE))
-                else:
-                    continue
-        elif user_input == "n":
-            print("Program will now exit.")
-            break
-        else:
-            print("invalid input, please try again.")
+    print_welcome_message()
+    print("First, let's get the current (wrong) date of one of the pictures.")
+    current_date = input_datetime_dialog()
+    print("Next, let's get the desired (correct) date of THE SAME PICTURE.")
+    desired_date = input_datetime_dialog()
+
+    print(f"I see {get_num_of_pics_in(os.listdir(os.getcwd()))} picture files in this folder.\nContinue? (y/n)")
+    if input() == "y":
+        for file in os.listdir(os.getcwd()):
+            if is_file_type_pic(file):
+                change_image_datetime(
+                    file,
+                    (current_date - desired_date))
+            else:
+                continue
+        print("\n\nScript Complete.\n\n")
+    else:
+        print("Script Stopped")
 
 if __name__ == "__main__":
     main()
